765dde2a47ca0e015404c21e2a0cf0b73f9da8e9
ChloeConstan <chloe.constan@oclock.school>
2024-03-19 16:56:52 +0100

    :lipstick: continued the characters icon

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -5,8 +5,7 @@ function Characters(){
         <section className='characters'>
             <article className='characters__descriptions'>
                 <h2 className='characters__title'>Aurora</h2>
-                <p className='characters__text'>Meet Aurora of Love in Zarzubay blabbl alblalb lalbldkdvs uoqdoudn oqd  nush qfisfb 
-                 jsfvjsdj nodsbo jvj nsd onsd v</p>
+                <p className='characters__text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non cupiditate pariatur maxime provident mollitia voluptates error temporibus ex dolorum voluptatem facere officia accusamus dolores molestias architecto ratione dolorem distinctio iusto suscipit quo eligendi maiores, ad recusandae quam. Voluptatum possimus ad quis odit obcaecati maxime dolorem aliquid esse nam doloremque ipsa cum ab recusandae quisquam, reiciendis consequatur at facere dicta adipisci totam non quia eos? Laboriosam, commodi. Laboriosam, optio dignissimos. Minima, soluta necessitatibus est unde accusamus, ad quasi consectetur qui omnis laudantium dicta enim, minus quibusdam vitae totam architecto iste molestiae! Fuga praesentium aspernatur quasi, odit porro exercitationem laborum. Ratione perferendis autem provident a necessitatibus in nobis explicabo facilis, eos vitae voluptas temporibus expedita officiis sed natus ipsam nam unde perspiciatis! Ipsa maiores, neque dolores, porro quis tenetur aliquam ab soluta labore optio hic ducimus explicabo voluptas. Dolor quam error, laborum facere, veritatis officia laudantium, optio aspernatur iusto atque et blanditiis?</p>
 
             </article>
             <article className='characters__pictures'>
@@ -14,7 +13,18 @@ function Characters(){
             
             </article>
             <article className='characters__button'>
-            
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/aurora_icon.png'/></button>
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/charlie_icon.png'/></button>
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/esteban_icon.png'/></button>
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/ethan_icon.png'/></button>
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/gaby_icon.png'/></button>
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/karim_icon.png'/></button>
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/leila_icon.png'/></button>
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/mirabella_icon.png'/></button>
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/nana_icon.png'/></button>
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/noah_icon.png'/></button>
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/sasha_icon.png'/></button>
+                <button className='characters__button--icon'><img className='characters__button--icon--img' src='public/img/theo_icon.png'/></button> 
             </article>
         </section>
     )
